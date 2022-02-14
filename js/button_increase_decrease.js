b4bb9aa070d450b82386ded2c58e19432b777b5d
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-13 23:02:05 -0600

    Created functions to do specific things, simple things like if statement to make it green or red. function for left and right button to increase/decrease the count display.

--- js/button_increase_decrease.js
+++ js/button_increase_decrease.js
@@ -1,16 +1,18 @@
-(function(){
 
-const lButton = document.getElementById('leftButton')
-const rButton = document.getElementById('rightButton')
 const bigNumber = document.getElementById('number')
 
     var count = 0;
     bigNumber.innerHTML = count;
 
-
-
-
-
+function leftwhenClicked(){
+    count--;
+    bigNumber.innerHTML = count;
+}
+function rightwhenClicked(){
+    count++;
+    document.getElementById('number').innerText = count;
+}
+function colorChange(){
     if (count < 0){
         bigNumber.style.color = 'red';
     } else if (count > 0){
@@ -18,5 +20,11 @@ const bigNumber = document.getElementById('number')
     } else{
         bigNumber.style.color = 'lightgray';
     }
+}
+var wow = document.getElementById('datePlacement')
+let date = new Date("02-13-2022");
 
-})()
+wow.innerText = (date.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}));
+
+
+
