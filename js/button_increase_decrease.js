46a87eea0f4b828777e8bb4669a5c4e6e42203e8
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-13 22:34:47 -0600

    grabed document ID's change color depending positive or negative

--- js/button_increase_decrease.js
+++ js/button_increase_decrease.js
@@ -1,12 +1,22 @@
 (function(){
 
+const lButton = document.getElementById('leftButton')
+const rButton = document.getElementById('rightButton')
+const bigNumber = document.getElementById('number')
+
+    var count = 0;
+    bigNumber.innerHTML = count;
 
 
 
 
 
-
-
-
+    if (count < 0){
+        bigNumber.style.color = 'red';
+    } else if (count > 0){
+        bigNumber.style.color = 'green';
+    } else{
+        bigNumber.style.color = 'lightgray';
+    }
 
 })()
